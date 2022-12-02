33159b1f14d854f2e7aa7048a31fa6b4cd15c1e1
Maxwell Volz <maxwellvolz@gmail.com>
2022-12-02 12:27:27 -0800

    late start on 2022; its advent time!!

---
+++ 01/part1.py
@@ -0,0 +1,31 @@
+"""
+"""
+
+f = open("data.txt")
+# f = open("example_data.txt")
+
+test_input = ''.join(f.readlines())
+calorie_data = test_input.split('\n\n')
+
+x = [z.split('\n') for z in calorie_data]
+
+calorie_sums = []
+
+most_calories = 0
+
+for y in x:
+    current_counter = 0
+
+    for calories in y:
+        current_counter += int(calories)
+
+    if current_counter > most_calories:
+        most_calories = current_counter
+
+    calorie_sums.append(current_counter)
+
+print(calorie_sums)
+print(most_calories)
+
+
+
